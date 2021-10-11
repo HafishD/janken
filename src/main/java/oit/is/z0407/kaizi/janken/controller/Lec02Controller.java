821d6bc23f503d8b6bcf7b1a8f4d0660e86ccdb9
HafishD <hafishdestarky@gmail.com>
2021-10-11 18:19:15 +0900

    CPUが手をランダムに出すように変更

--- src/main/java/oit/is/z0407/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0407/kaizi/janken/controller/Lec02Controller.java
@@ -3,9 +3,10 @@ package oit.is.z0407.kaizi.janken.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z0407.kaizi.janken.model.Janken;
 
 @Controller
 public class Lec02Controller {
@@ -23,17 +24,37 @@ public class Lec02Controller {
 
   @GetMapping("/lec02janken")
   public String lec02Janken(@RequestParam String hand, ModelMap model) {
-    String result;
-    if (hand.equals("Pa")) {
-      result = "You Win!";
-    } else if (hand.equals("Choki")) {
-      result = "You Lose...";
-    } else if (hand.equals("Gu")) {
+    String result = "";
+    Janken janken = new Janken();
+    if (hand.equals(janken.getCpuHand())) {
       result = "It's a Draw.";
     } else {
-      result = "NO MATCH";
+      switch (hand) {
+        case "Gu":
+          if (janken.getCpuHand().equals("Choki")) {
+            result = "You Win!";
+          } else if (janken.getCpuHand().equals("Pa")) {
+            result = "You Lose...";
+          }
+          break;
+        case "Choki":
+          if (janken.getCpuHand().equals("Pa")) {
+            result = "You Win!";
+          } else if (janken.getCpuHand().equals("Gu")) {
+            result = "You Lose...";
+          }
+          break;
+        case "Pa":
+          if (janken.getCpuHand().equals("Gu")) {
+            result = "You Win!";
+          } else if (janken.getCpuHand().equals("Choki")) {
+            result = "You Lose...";
+          }
+          break;
+      }
     }
     model.addAttribute("hand", hand);
+    model.addAttribute("janken", janken);
     model.addAttribute("result", result);
     return "lec02.html";
   }
