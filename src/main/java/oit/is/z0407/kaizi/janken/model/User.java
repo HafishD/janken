25b56bbc6e5ecb89e7bfcb468e053e0acf99489a
HafishD <hafishdestarky@gmail.com>
2021-10-22 18:30:00 +0900

    ユーザ情報をSELECTで取得して表示する

--- src/main/java/oit/is/z0407/kaizi/janken/model/User.java
+++ src/main/java/oit/is/z0407/kaizi/janken/model/User.java
@@ -1,5 +1,22 @@
 package oit.is.z0407.kaizi.janken.model;
 
 public class User {
-  
+  int id;
+  String name;
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String user) {
+    this.name = user;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
