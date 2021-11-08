eb67686c604cf2a2475b737b311144064a1213a5
HafishD <hafishdestarky@gmail.com>
2021-11-08 18:27:36 +0900

    対戦相手が手を選び非同期処理で結果を表示する

--- src/main/java/oit/is/z0407/kaizi/janken/model/MatchInfoMapper.java
+++ src/main/java/oit/is/z0407/kaizi/janken/model/MatchInfoMapper.java
@@ -12,12 +12,30 @@ public interface MatchInfoMapper {
   @Insert("INSERT INTO matchInfo (user1, user2, user1Hand, isActive) VALUES (#{user1}, #{user2}, #{user1Hand}, #{isActive});")
   void insertMatchInfo(MatchInfo matchInfo);
 
+  // @Select("SELECT * FROM MATCHINFO;")
+  // ArrayList<MatchInfo> selectMatchInfos();
+
   @Select("SELECT id from MATCHINFO where isActive = TRUE;")
   int activeMatchId();
 
   @Select("SELECT * FROM MATCHINFO where isActive = TRUE;")
   ArrayList<MatchInfo> selectActiveMatchInfos();
 
-  @Update("UPDATE MATCHINFO SET USER1HAND=#{user1Hand} WHERE ISACTIVE = #{isActive}")
+  @Select("SELECT count(*) FROM MATCHINFO where user2 = #{find} and isActive = TRUE;")
+  int findMyMatch(int find);
+
+  @Select("SELECT count(*) FROM MATCHINFO where ISACTIVE = TRUE;")
+  int findActive();
+
+  // @Select("SELECT * FROM MATCHINFO where user2 = #{id} and isActive = TRUE;")
+  // ArrayList<MatchInfo> selectMyMatch(int id);
+
+  @Select("SELECT user1Hand FROM MATCHINFO where user2 = #{id} and isActive = TRUE;")
+  String selectUser2Hand(int id);
+
+  @Update("UPDATE MATCHINFO SET USER1HAND=#{user1Hand} WHERE ISACTIVE = #{isActive} and user1 = #{user1}")
   void updateById(MatchInfo matchInfo);
+
+  @Update("UPDATE MATCHINFO SET ISACTIVE = FALSE where ISACTIVE = TRUE")
+  void updateMatchInfo();
 }
--- src/main/java/oit/is/z0407/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z0407/kaizi/janken/model/MatchMapper.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MatchMapper {
@@ -12,6 +13,18 @@ public interface MatchMapper {
   @Select("SELECT * from matches;")
   ArrayList<Match> selectAllMatches();
 
+  @Select("SELECT * FROM MATCHES where isActive = TRUE;")
+  Match selectActiveMatch();
+
+  @Select("SELECT count(*) FROM MATCHES where isActive = TRUE;")
+  int findActiveMatch();
+
+  @Select("SELECT ID FROM MATCHES where isActive = TRUE;")
+  int findMatchId();
+
   @Insert("INSERT INTO matches (user1, user2, user1Hand, user2Hand, isActive) VALUES (#{user1}, #{user2}, #{user1Hand}, #{user2Hand}, #{isActive});")
   void insertMatch(Match match);
+
+  @Update("UPDATE MATCHES SET ISACTIVE = FALSE where ISACTIVE = TRUE;")
+  void updateMatch();
 }
